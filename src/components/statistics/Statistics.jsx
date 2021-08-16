cd4dd05dd3a50c09ea8f6444b0820ee2c811342e
koliadjun <koliadjun@gmail.com>
2021-08-16 23:56:45 +0300

    added style for statistics

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -4,12 +4,36 @@ const Statistics = ({ title, stats }) => (
   <section className={s.statistics}>
     {title && <h2 className={s.title}>{title}</h2>}
     <ul className={s.statList}>
-      {stats.map(stat => (
-        <li className={s.item} key={stat.id}>
-          <span className={s.label}>{stat.label}</span>
-          <span className={s.percentage}>{stat.percentage}</span>
-        </li>
-      ))}
+      {stats.map(stat => {
+        let color;
+        switch (true) {
+          case stat.percentage < 10:
+            color = '#ff0000';
+            break;
+          case stat.percentage < 20:
+            color = '#ff8e00';
+            break;
+          case stat.percentage < 30:
+            color = '#ffe100';
+            break;
+          case stat.percentage > 30:
+            color = '#00ff00';
+            break;
+          default:
+            color = '#00ff00';
+            break;
+        }
+        return (
+          <li
+            className={s.item}
+            style={{ backgroundColor: ` ${color}` }}
+            key={stat.id}
+          >
+            <span className={s.label}>{stat.label}</span>
+            <span className={s.percentage}>{stat.percentage}&#37;</span>
+          </li>
+        );
+      })}
     </ul>
   </section>
 );
